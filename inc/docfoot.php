3dd8a274a843b6fe0486cd293c15d6b480a7385a
mk-kimjh <mk.kimjh@gmail.com>
2017-08-24 10:02:33 +0900

    레이아웃 작성 중...

---
+++ inc/docfoot.php
@@ -0,0 +1,2 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="/assets/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+</head>
+<body>
+	<div id="top-nav" class="visible-lg">
+		<div class="container">
+			<ul class="sns">
+				<li><a href="#">페이스북</a></li>
+				<li><a href="#">다음</a></li>
+			</ul>
+			<ul class="util">
+				<li><a href="#">로그인</a></li>
+				<li><a href="#">회원가입</a></li>
+				<li><a href="#">정보수정</a></li>
+			</ul>
+		</div>
+	</div>
+	<header id="header">
+		<h1 class="logo"><a href="/">한국대학원리연구회</a></h1>
+	</header>
+	<main id="content">
+
+	</main>
+	<footer id="footer">
+
+	</footer>
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+</body>
+</html>
