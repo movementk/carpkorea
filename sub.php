54c5ff59ac23c37ebaaa84c809e41fe6a95d6494
kimyh5392 <mk.kimyh@gmail.com>
2017-08-24 18:02:59 +0900

    sub 페이지 레이아웃 영역 및 페이지 헤더 작업 완료.

---
+++ sub.php
@@ -0,0 +1,87 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+</head>
+<body>
+	<div id="top-nav" class="visible-lg">
+		<div class="container">
+			<ul class="sns">
+				<li><a class="fb" href="#" target="_blank"><span class="sr-only">페이스북</span></a></li>
+				<li><a class="dc" href="#" target="_blank"><span class="sr-only">다음</span></a></li>
+			</ul>
+			<ul class="util">
+				<li><a href="#">로그인</a></li>
+				<li><a href="#">회원가입</a></li>
+				<li><a href="#">정보수정</a></li>
+			</ul>
+		</div>
+	</div>
+	<header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<h1 class="logo"><a href="/"><span class="sr-only">한국대학원리연구회</span></a></h1>
+					<button class="btn btn-link btn-nav-open hidden-lg"><span class="sr-only">메뉴 열기</span></button>
+				</div>
+				<div class="col-lg-9 visible-lg">
+				</div>
+			</div>
+		</div>
+	</header>
+    <!-- Content Start -->
+	<main id="content">
+        <!-- Page-Header -->
+        <div class="page-header">
+            <h2 class="header-title">CARP</h2>
+            <div class="container">
+                <h3 class="page-title">CARP 소개</h3>
+                <nav class="sub-snb">
+                    <ul>
+                        <li class="active"><a href="#">창립목적</a></li>
+                        <li><a href="#">VISION MISSION</a></li>
+                        <li><a href="#">CI</a></li>
+                        <li class="space"><a href="#">향후 나아갈 길<br class="visible-xs"> Action 100</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <section>
+            <div class="container">
+                <div class="section-header">
+                    <h4 class="title">창립목적</h4>
+                </div>
+                <div class="section-content">
+                    <p class="page-summary">
+                        본인이 <i>CARP</i>를 창설하게 된 동기중 
+                        의 하나는<br class="hidden-xs"> 대학생들의 그 <i>젊음과 열정</i>
+                        에 있습니다.
+                    </p>
+                </div>
+            </div>
+        </section>
+	</main>
+	<!-- Content End -->
+	<nav id="policy">
+		<div class="container">
+			<ul>
+				<li class="hidden-xs"><a href="#">연구회 소개</a></li>
+				<li class="hidden-xs"><a href="#">위치 및 연락처</a></li>
+				<li><a href="#">이용약관</a></li>
+				<li><a href="#">개인정보취급방침</a></li>
+				<li><a href="#">이메일수집무단거부</a></li>
+			</ul>
+		</div>
+	</nav>
+	<footer id="footer">
+		<div class="container">
+			<address>04309 서울시 용산구 청파로 47나길 28 (청파동 2가 9-1) 한국대학원리연구회</address>
+			<ul class="contact">
+				<li>대표전화 : 02-718-3732</li>
+				<li>팩스 : 02-718-3521</li>
+				<li>E-mail : <a href="mailto:k-carphq@hanmail.net" target="_blank">k-carphq@hanmail.net</a></li>
+			</ul>
+			<p class="copyright">(C)2017 한국대학원리연구회. ALL RIGHT RESERVED. <br class="visible-xs">SITE BY <a href="//movementk.com" target="_blank">MOVEMENT K GROUP</a>.</p>
+		</div>
+	</footer>
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+</body>
+</html>
