e4e1076c0bd441ad913a9b9166a985ad382333c2
kimyh5392 <mk.kimyh@gmail.com>
2017-08-28 09:24:30 +0900

    메인 퍼블리싱 작업중 . .

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
                 </li>
             </ul>
         </div>
-        <div class="declare">
+        <section class="declare">
             <div class="declare-content">
                 <div class="container">
                     <h2>참부모선포</h2>
@@ -98,8 +98,8 @@
                     50%
                 </div>
             </div>
-        </div>
-        <div class="information">
+        </section>
+        <section class="information">
             <div class="container">
                 <div class="row">
                     <div class="col-xs-4">
@@ -107,7 +107,7 @@
                             <!-- 모바일 및 테블릿 -->
                             <a href="#" class="hidden-md hidden-lg link-box">
                                 <p class="figure">
-                                    <img src="/assets/images/main/ico_news.png" alt="">
+                                    <img src="/assets/images/main/ico_news.png" alt="CARP NEWS">
                                 </p>
                                 <p class="details">CARP NEWS</p>
                             </a>
@@ -161,12 +161,13 @@
                             <!-- 모바일 및 테블릿 -->
                             <a href="#" class="hidden-md hidden-lg link-box">
                                 <p class="figure">
-                                    <img src="/assets/images/main/ico_schedule.png" alt="">
+                                    <img src="/assets/images/main/ico_schedule.png" alt="일정 보기">
                                 </p>
                                 <p class="details">일정 보기</p>
                             </a>
                             <!-- web 버전 -->
                             <div class="hidden-xs hidden-sm">
+                                <!-- 달력 -->
                                 <div class="calendar">
                                     <div id="schedule"></div>
                                 </div>
@@ -178,7 +179,7 @@
                             <!-- 모바일 및 테블릿 -->
                             <a href="#" class="hidden-md hidden-lg link-box">
                                 <p class="figure">
-                                    <img src="/assets/images/main/ico_cms.png" alt="">
+                                    <img src="/assets/images/main/ico_cms.png" alt="CMS 후원">
                                 </p>
                                 <p class="details">CMS 후원</p>
                             </a>
@@ -189,13 +190,186 @@
                                     <p>
                                         기부금 전액은 원리연구회의 발전기금으로 사용되며 사회에 공헌하는 다양한 행사에 가치있게 사용됩니다.
                                     </p>
+                                    <ul class="row">
+                                        <li class="col-xs-6">
+                                            <a href="#">
+                                                <figure>
+                                                    <img src="/assets/images/main/ico_individual.png" alt="개인 후원신청">
+                                                    <figcaption>개인 후원신청<br>바로가기</figcaption>
+                                                </figure>
+                                            </a>
+                                        </li>
+                                        <li class="col-xs-6">
+                                            <a href="#">
+                                                <figure>
+                                                    <img src="/assets/images/main/ico_company.png" alt="기업 후원신청">
+                                                    <figcaption>기업 후원신청<br>바로가기</figcaption>
+                                                </figure>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+        <section class="carp-section">
+            <div class="container">
+                <h2>CARP Action 100</h2>
+                <p class="summary">
+                    CARP는 50년 역사를 토대로 새로운 50년을 향한
+                    100년의 미래를 그리며<br class="hidden-xs"> 인류의 미래를 희망으로 
+                    이끌 수 있는 영성과 지성을 겸비한 창조적 지도자
+                    양성을 추구할 것입니다. 
+                </p>
+                <ul class="action-list">
+                    <li>
+                        <figure>
+                            <img src="/assets/images/main/action_img01.png" class="img-responsive" alt="">
+                            <figcaption>통일원리 연구기관<br>으로서의 역량강화</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="/assets/images/main/action_img02.png" class="img-responsive" alt="">
+                            <figcaption>세계평화를 위한<br>남북통일 운동</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="/assets/images/main/action_img03.png" class="img-responsive" alt="">
+                            <figcaption>심정문화 장착을<br>위한 지원확대</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="/assets/images/main/action_img04.png" class="img-responsive" alt="">
+                            <figcaption>평화세계를 이끌어<br>나갈 지도자 양성</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="/assets/images/main/action_img05.png" class="img-responsive" alt="">
+                            <figcaption>참부모 선포운동</figcaption>
+                        </figure>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="board-list">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <div class="video">
+                            <h3 class="title">
+                                영상게시판
+                                <a href="#">
+                                    <i class="icon-plus"><span class="sr-only">더보기</span></i>
+                                </a>
+                            </h3>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CGq7eI7Y9Gw"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="photo-list">
+                            <h3 class="title">
+                                사진게시판
+                                <a href="#">
+                                    <i class="icon-plus"><span class="sr-only">더보기</span></i>
+                                </a>
+                            </h3>
+                            <ul class="row">
+                                <li class="col-xs-12 col-sm-6">
+                                    <a href="#">
+                                        <p class="figure">
+                                            <img src="/assets/images/main/photo_img01.jpg" class="img-responsive" alt="">
+                                        </p>
+                                        <div class="details">
+                                            <h4>사진게시판 제목 노출영역01</h4>
+                                            <p class="summary">
+                                                사진 게시판에 등록된 게시글의 상세내용이 노출되
+                                                니다.사진 게시판에 등록된  게시글의 상세내용이
+                                                영역입니다. 사진 게시판에 등록된 게시글의 ...
+                                            </p>
+                                            <ul>
+                                                <li class="count">
+                                                    <p><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</p>
+                                                </li>
+                                                <li class="comment">
+                                                    <p><i class="icon-comment"><span class="sr-only">댓글</span></i>21</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="col-sm-6 hidden-xs">
+                                    <a href="#">
+                                        <p class="figure">
+                                            <img src="/assets/images/main/photo_img02.jpg" class="img-responsive" alt="">
+                                        </p>
+                                        <div class="details">
+                                            <h4>사진게시판 제목 노출영역02</h4>
+                                            <p class="summary">
+                                                사진 게시판에 등록된 게시글의 상세내용이 노출되
+                                                니다.사진 게시판에 등록된  게시글의 상세내용이
+                                                영역입니다. 사진 게시판에 등록된 게시글의 ...
+                                            </p>
+                                            <ul>
+                                                <li class="count">
+                                                    <p><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</p>
+                                                </li>
+                                                <li class="comment">
+                                                    <p><i class="icon-comment"><span class="sr-only">댓글</span></i>21</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="event-list">
+            <div class="row">
+                <div class="col-xs-12 col-md-6">
+                    <ul class="row">
+                        <li class="col-xs-12 col-sm-6">
+                            <p class="event-item">
+                                <img src="" alt="">
+                                <a href="#">바로가기<i class="icon-right-dir"></i></a>
+                            </p>
+                        </li>
+                        <li class="col-xs-12 col-sm-6">
+                            <p class="event-item">
+                                <img src="" alt="">
+                                <a href="#">바로가기<i class="icon-right-dir"></i></a>
+                            </p>
+                        </li>
+                        <li class="col-xs-12 col-sm-6">
+                            <p class="event-item">
+                                <img src="" alt="">
+                                <a href="#">바로가기<i class="icon-right-dir"></i></a>
+                            </p>
+                        </li>
+                        <li class="col-xs-12 col-sm-6">
+                            <p class="event-item">
+                                <img src="" alt="">
+                                <a href="#">바로가기<i class="icon-right-dir"></i></a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    111
+                </div>
+            </div>
+        </section>
 	</main>
 	
 	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
@@ -240,8 +414,8 @@
             //일정표 달력
             $( "#schedule" ).datepicker({
                 dateFormat: 'yy-mm-dd',
-                prevText: '이전 달',
-                nextText: '다음 달',
+                prevText: '<span class="ui-icon ui-icon-circle-triangle-w"><i class="icon-left-open"><span class="sr-only">이전달</span></i></span>',
+                nextText: '<span class="ui-icon ui-icon-circle-triangle-e"><i class="icon-right-open"><span class="sr-only">다음달</span></i></span>',
                 monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
                 monthNamesShort: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
                 showMonthAfterYear: true,
