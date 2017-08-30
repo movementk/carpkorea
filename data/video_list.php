0f9b8401f549c5ad2361232ae7ed55b021297f22
kimyh5392 <mk.kimyh@gmail.com>
2017-08-30 18:53:41 +0900

    carp 광장 작업중. .

---
+++ data/video_list.php
@@ -0,0 +1,213 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/data.css" rel="stylesheet">
+</head>
+<body class="data"> <!-- Class 폴더 -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+   
+    <!-- Content Start -->
+	<main id="content">
+        <!-- Page-Header -->
+        <div class="page-header">
+            <h2 class="header-title">자료실</h2>
+            <div class="container">
+                <h3 class="page-title">영상게시판</h3>
+            </div>
+        </div>
+        <article class="common-list video-list">
+            <div class="container">
+                <!-- 검색폼 -->
+                <div class="search-form">
+                    <form action="#">
+                        <div class="form-group">
+                            <select class="form-control">
+                                <option>제목</option>
+                            </select>
+                            <label for="search" class="sr-only">검색어</label>
+                            <input type="text" id="search" class="form-control">
+                        </div>
+                        <p class="btn-search">
+                            <button type="submit" class="btn">검색</button>
+                        </p>
+                    </form>
+                </div>
+                <ul class="row">
+                    <li class="col-xs-12 col-sm-4 col-md-3">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3 hidden-xs">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3 hidden-xs">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3 hidden-xs hidden-sm">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="col-xs-12 col-sm-4 col-md-3 hidden-xs hidden-sm">
+                        <a href="/data/photo_view.php">
+                            <p class="item">
+                                <img src="/assets/images/data/photo_list_img01.jpg" class="img-responsive" alt="">
+                            </p>
+                            <div class="details">
+                                <h4 class="title">사진게시판 제목 노출영역01</h4>
+                                <p>
+                                    사진 게시판에 등록된 게시글의 상세 내용이 노출되는 영역입니다.
+                                </p>
+                                <ul>
+                                    <li><i class="icon-eye"><span class="sr-only">조회수</span></i>1,234</li>
+                                    <li><i class="icon-comment"><span class="sr-only">댓글수</span></i>21</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <nav class="paging" aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <i class="icon-angle-double-left"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <i class="icon-angle-left"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li class="hidden-xs"><a href="#">6</a></li>
+                        <li class="hidden-xs"><a href="#">7</a></li>
+                        <li class="hidden-xs"><a href="#">8</a></li>
+                        <li class="hidden-xs"><a href="#">9</a></li>
+                        <li class="hidden-xs"><a href="#">10</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <i class="icon-angle-right"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <i class="icon-angle-double-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </article>
+	</main>
+	
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+</body>
+</html>
---
+++ square/carp_list.php
@@ -0,0 +1,44 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/square.css" rel="stylesheet">
+</head>
+<body class="square"> <!-- Class 폴더 -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+   
+    <!-- Content Start -->
+	<main id="content">
+        <!-- Page-Header -->
+        <div class="page-header">
+            <h2 class="header-title">CARP 광장</h2>
+        </div>
+        <article class="carp-list">
+            <div class="container">
+                <div class="facebook">
+                    <div class="facebook-content">
+                        
+                    </div>
+                </div>
+                <nav class="link-list">
+                    <ul>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">6</a></li>
+                        <li><a href="#">7</a></li>
+                    </ul>
+                    <p class="people-img01">
+                        <img src="/assets/images/square/people_xs_01.png" alt="">
+                    </p>
+                </nav>
+                <p class="people-img02"></p>
+            </div>
+            <div class="article-foot"></div>
+        </article>
+	</main>
+	
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+</body>
+</html>
